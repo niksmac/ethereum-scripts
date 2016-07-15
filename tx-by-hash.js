98cab296cb975fb648c64e98473db05bd7ccd3ba
niksmac <nikhil@lightrains.com>
2016-07-15 12:21:00 +0530

    Added printTransaction(txHash)

---
+++ tx-by-hash.js
@@ -0,0 +1,21 @@
+/*
+ * Print transaction details from a given transaction hash
+ * http://ethereum.stackexchange.com/a/2936/259
+ */
+
+function printTransaction(txHash) {
+  var tx = eth.getTransaction(txHash);
+  if (tx != null) {
+    console.log("  tx hash          : " + tx.hash + "\n"
+      + "   nonce           : " + tx.nonce + "\n"
+      + "   blockHash       : " + tx.blockHash + "\n"
+      + "   blockNumber     : " + tx.blockNumber + "\n"
+      + "   transactionIndex: " + tx.transactionIndex + "\n"
+      + "   from            : " + tx.from + "\n"
+      + "   to              : " + tx.to + "\n"
+      + "   value           : " + tx.value + "\n"
+      + "   gasPrice        : " + tx.gasPrice + "\n"
+      + "   gas             : " + tx.gas + "\n"
+      + "   input           : " + tx.input);
+  }
+}
